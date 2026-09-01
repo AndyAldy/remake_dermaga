b66deb801c634394dc07fc6e693a5c726042c10e
AndyAldy <andyaldyansyah11@gmail.com>
2026-09-01 08:15:22 +0700

    edited addresses

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-slate-900 text-slate-400 py-10 text-center text-sm px-6">
     <p>&copy; 2026 Badan Pusat Statistik Kota Semarang. Semua Hak Dilindungi.</p>
-    <p className="mt-2">Jl. W.R. Supratman No.114, Semarang Barat.</p>
+    <p className="mt-2">Jalan Inspeksi Kali semarang No.1, Sekayu, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50132</p>
   </footer>
 );
 
